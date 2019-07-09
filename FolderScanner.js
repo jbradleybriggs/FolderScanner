a2602592cd52794527b6eb9020f07bce74941dc5
jason.briggs <jason.briggs@techss.co.za>
2019-07-09 15:29:13 +0200

    Started asynchronous folder scanning

--- FolderScanner.js
+++ FolderScanner.js
@@ -47,25 +47,72 @@
     }
 
     async getFiles(recursive) {
+        //return new Promise((resolve, reject) => {
+        /* this.fileList = [];
+         await this.fs.readdir(this.folderPath, { withFileTypes: true }, (err, files) => {
+             return new Promise((resolve, reject) => {
+                 if (err) reject(err);
+                 else {
+                     var getAll = this.filter.includes(".*");
+ 
+                     if (files.length > 0) {
+                         for (var index in files) {
+                             var path = this.folderPath + this.slash + files[index].name;
+                             //console.log(path) ;
+ 
+                             if (files[index].isDirectory() && recursive) {
+                                 var innerFs = new FolderScanner(path, this.filter);
+                                 //console.log(path);
+                                 innerFs.getFiles(recursive).then((data) => {
+                                     this.fileList = this.fileList.concat(data); //append these
+                                 });
+                                 //this.fileList.concat(innerFs.getFilesSync(true)) ;
+                             }
+                             else if (files[index].isFile()) {
+                                 if (!getAll) {
+                                     var ext = path.substring(path.lastIndexOf(".")).toLowerCase();
+                                     if (this.filter.includes(ext)) {
+                                         this.fileList.push(path);
+                                     }
+                                 }
+                                 else this.fileList.push(path);
+                             }
+                         }
+                         //console.log(this.fileList);
+                         resolve(this.fileList);
+                     }
+                 }
+                 // console.log(this.fileList) ;
+                 resolve(this.fileList);
+             });
+         });
+         console.log("H" + this.fileList);
+         return this.fileList;*/
+        //}//)
+
+        this.fileList = [];
         return new Promise((resolve, reject) => {
-            this.fileList = [];
-            var files = this.fs.readdir(this.folderPath, { withFileTypes: true }, (err, files) => {
+            this.fs.readdir(this.folderPath, { withFileTypes: true }, (err, files) => {
                 if (err) reject(err);
                 else {
+                    //resolve in here
+                    // resolve(JSON.stringify(files));
                     var getAll = this.filter.includes(".*");
-
                     if (files.length > 0) {
                         for (var index in files) {
                             var path = this.folderPath + this.slash + files[index].name;
-                            //console.log(path) ;
-
+                            //this.fileList.push(path);
                             if (files[index].isDirectory() && recursive) {
-                                var innerFs = new FolderScanner(path, this.filter);
-                                console.log(path);
-                                innerFs.getFiles(recursive).then((data) => {
-                                    this.fileList = this.fileList.concat(data); //append these
-                                });
-                                //this.fileList.concat(innerFs.getFilesSync(true)) ;
+                                //recursively scan through these directories
+                                var inner = new FolderScanner(path, this.filter);
+                                (async () => {
+                                    //console.log("before");
+                                    await inner.getFiles(true).then((list) => {
+                                        this.fileList = this.fileList.concat(list);
+                                        //console.log(list)
+                                    });
+                                    //console.log(this.fileList);
+                                })();
                             }
                             else if (files[index].isFile()) {
                                 if (!getAll) {
@@ -77,13 +124,11 @@
                                 else this.fileList.push(path);
                             }
                         }
-                        resolve(this.fileList);
                     }
+                    resolve(this.fileList);
                 }
-                // console.log(this.fileList) ;
-                resolve(this.fileList);
             });
-        });
+        })
     }
 
     saveToFile(filePath) {
@@ -94,7 +139,7 @@
     }
 }
 
-var fscan = new FolderScanner("/home/jason/Videos", [".mp4", ".mkv", ".avi", ".flv", ".mpg", ".rm", ".webm"]);
+var fscan = new FolderScanner("/home/jason/Videos", [".mp4", ".mkv", ".avi", ".flv", ".mpg", ".rm", ".webm", ".rmvb"]);
 fscan.getFiles(true).then((list) => {
-    console.log("THE LIST" + list);
+    console.log(list);
 });
