5f224d08c36d7006a859ce619df89dbcac64a83f
jason.briggs <jason.briggs@techss.co.za>
2019-07-10 10:52:39 +0200

    Added async version of getFiles()

--- FolderScanner.js
+++ FolderScanner.js
@@ -3,7 +3,7 @@
 *
 * @author J. Bradley Briggs
 */
-/*module.exports = */class FolderScanner {
+module.exports = class FolderScanner {
     /**
      * 
      * @param {string} folderPath 
@@ -12,6 +12,7 @@
     constructor(folderPath, filter) {
         this.fs = require('fs');
         this.folderPath = folderPath;
+        if (!filter || (typeof filter == 'object' && filter.length == 0)) filter = ['.*'];
         this.filter = filter;
         if (process.platform == "win32") this.slash = "\\";
         else this.slash = '/';
@@ -46,88 +47,44 @@
         return this.fileList;
     }
 
-    async getFiles(recursive) {
-        //return new Promise((resolve, reject) => {
-        /* this.fileList = [];
-         await this.fs.readdir(this.folderPath, { withFileTypes: true }, (err, files) => {
-             return new Promise((resolve, reject) => {
-                 if (err) reject(err);
-                 else {
-                     var getAll = this.filter.includes(".*");
- 
-                     if (files.length > 0) {
-                         for (var index in files) {
-                             var path = this.folderPath + this.slash + files[index].name;
-                             //console.log(path) ;
- 
-                             if (files[index].isDirectory() && recursive) {
-                                 var innerFs = new FolderScanner(path, this.filter);
-                                 //console.log(path);
-                                 innerFs.getFiles(recursive).then((data) => {
-                                     this.fileList = this.fileList.concat(data); //append these
-                                 });
-                                 //this.fileList.concat(innerFs.getFilesSync(true)) ;
-                             }
-                             else if (files[index].isFile()) {
-                                 if (!getAll) {
-                                     var ext = path.substring(path.lastIndexOf(".")).toLowerCase();
-                                     if (this.filter.includes(ext)) {
-                                         this.fileList.push(path);
-                                     }
-                                 }
-                                 else this.fileList.push(path);
-                             }
-                         }
-                         //console.log(this.fileList);
-                         resolve(this.fileList);
-                     }
-                 }
-                 // console.log(this.fileList) ;
-                 resolve(this.fileList);
-             });
-         });
-         console.log("H" + this.fileList);
-         return this.fileList;*/
-        //}//)
-
-        this.fileList = [];
+    async readdir(path) {
         return new Promise((resolve, reject) => {
-            this.fs.readdir(this.folderPath, { withFileTypes: true }, (err, files) => {
-                if (err) reject(err);
-                else {
-                    //resolve in here
-                    // resolve(JSON.stringify(files));
-                    var getAll = this.filter.includes(".*");
-                    if (files.length > 0) {
-                        for (var index in files) {
-                            var path = this.folderPath + this.slash + files[index].name;
-                            //this.fileList.push(path);
-                            if (files[index].isDirectory() && recursive) {
-                                //recursively scan through these directories
-                                var inner = new FolderScanner(path, this.filter);
-                                (async () => {
-                                    //console.log("before");
-                                    await inner.getFiles(true).then((list) => {
-                                        this.fileList = this.fileList.concat(list);
-                                        //console.log(list)
-                                    });
-                                    //console.log(this.fileList);
-                                })();
-                            }
-                            else if (files[index].isFile()) {
-                                if (!getAll) {
-                                    var ext = path.substring(path.lastIndexOf(".")).toLowerCase();
-                                    if (this.filter.includes(ext)) {
-                                        this.fileList.push(path);
-                                    }
-                                }
-                                else this.fileList.push(path);
-                            }
+            this.fs.readdir(path, { withFileTypes: true }, (err, files) => {
+                if (err) resolve([]); //don't reject - merely resolve with nothing
+                resolve(files);
+            })
+        });
+    }
+
+    async __getFiles(path, recursive) {
+        var fileList = [];
+        var files = await this.readdir(path);
+        var getAll = this.filter.includes(".*");;
+
+        if (files.length > 0) {
+            for (var index in files) {
+                var subPath = path + this.slash + files[index].name;
+                if (files[index].isDirectory() && recursive) {
+                    var subFileList = await this.__getFiles(subPath, recursive);
+                    fileList = fileList.concat(subFileList); //append these
+                }
+                else if (files[index].isFile()) {
+                    if (!getAll) {
+                        var ext = subPath.substring(subPath.lastIndexOf(".")).toLowerCase();
+                        if (this.filter.includes(ext)) {
+                            fileList.push(subPath);
                         }
                     }
-                    resolve(this.fileList);
+                    else fileList.push(subPath);
                 }
-            });
+            }
+        }
+        return fileList;
+    }
+
+    async getFiles(recursive) {
+        return new Promise((resolve, reject) => {
+            resolve(this.__getFiles(this.folderPath, recursive));
         })
     }
 
@@ -139,7 +96,5 @@
     }
 }
 
-var fscan = new FolderScanner("/home/jason/Videos", [".mp4", ".mkv", ".avi", ".flv", ".mpg", ".rm", ".webm", ".rmvb"]);
-fscan.getFiles(true).then((list) => {
-    console.log(list);
-});
+// var fscan = new FolderScanner("/home/jason/Downloads", []);
+// fscan.getFiles(true).then(list => console.log(list));
